288b2de28235a6e150add5771bdeb597debcefca
fofinhoinsider <andrei.sgi97@gmail.com>
2023-03-01 02:34:27 -0300

    test deploy automation

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,7 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <p>Testing Prettier</p>
+                    <p>Companies</p>
                     <a
                         className="App-link"
                         href="https://reactjs.org"
