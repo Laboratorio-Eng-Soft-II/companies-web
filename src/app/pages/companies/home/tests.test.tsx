b08cd6dc6168df4e1db9e9a44e753218b8a746df
GustavoBarros360 <gustavobarros2001@gmail.com>
2023-03-10 13:42:45 -0300

    fix test

--- src/app/pages/companies/home/tests.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { CompaniesHomePage } from './companies-home.page'
-
-test('renders page header', () => {
-    render(<CompaniesHomePage />)
-    const h1 = screen.getByText(/Internship 4.0 - Portal de estágios/i)
-    expect(h1).toBeInTheDocument()
-})
