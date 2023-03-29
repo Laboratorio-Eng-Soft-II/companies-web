83f22ea0e3deb4e2bf09eb6c7335b895227f1f57
GustavoBarros360 <gustavobarros2001@gmail.com>
2023-03-28 23:21:28 -0300

    remove test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import App from './App'
 
 test('renders learn react link', () => {
-    render(<App />)
+    // render(<App />)
     // const linkElement = screen.getByText(/Test add typescript/i)
     // expect(linkElement).toBeInTheDocument();
 })
